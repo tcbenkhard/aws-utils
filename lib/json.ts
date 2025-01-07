e93ba47d6396955c4f3bc77d681af818a61b0b2d
Timo Benkhard <tcbenkhard@gmail.com>
2025-01-07 12:40:51 +0100

    Add extra to JSON parsing

--- lib/json.ts
+++ lib/json.ts
@@ -3,11 +3,11 @@ import {ServerError} from "./errors";
 
 export class ParsingException extends Error {}
 
-export const parseBody = (body: string | null, schema: ZodSchema<any>) => {
+export const parseBody = (body: string | null, schema: ZodSchema<any>, extras: Record<string, string> = {}) => {
     if(!body) throw ServerError.badRequest("MISSING_BODY", "Missing body")
     try {
         const jsonBody = JSON.parse(body)
-        return schema.parse(jsonBody)
+        return schema.parse({...jsonBody, ...extras})
     } catch (e) {
         throw ServerError.badRequest("INVALID_REQUEST_BODY", "Failed to process request body")
     }
--- test/json.test.ts
+++ test/json.test.ts
@@ -1,6 +1,12 @@
 import {parseBody} from "../lib/json";
 import {z} from "zod";
 import {ServerError} from "../lib";
+import exp = require("node:constants");
+
+const objectWithExtras = z.object({
+    name: z.string(),
+    extraName: z.string()
+})
 
 describe('Json utils', () => {
     it('should raise on missing body', () => {
@@ -14,5 +20,16 @@ describe('Json utils', () => {
     it('should parse json', () => {
         const result = parseBody('{"name": "test"}', z.object({name: z.string()}))
         expect(result).toEqual({name: "test"})
+    })
+
+    it('should add extras', () => {
+        const result = parseBody('{"name": "test"}', objectWithExtras, {
+            extraName: 'superExtra'
+        })
+
+        expect(result).toEqual({
+            name: "test",
+            extraName: 'superExtra'
+        })
     })
 })
